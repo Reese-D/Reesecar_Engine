12ed8d607d18196a682344a856536828d40086d0
Reese De Wind <Reese.DeWind@gmail.com>
2024-03-29 03:53:20 -0400

    Adding necessary crates and engine skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use log::{info, trace, warn};
+
+struct Engine
+{
+
+}
+
+impl Engine {
+
+    fn new() -> Self {
+	info!("creating engine instance");
+	Engine::init_vulkan();
+	Engine::main_loop();
+	Engine::cleanup();
+
+	Self{}
+    }
+    fn init_vulkan(){
+
+    }
+
+    fn main_loop(){
+
+    }
+
+    fn cleanup(){
+	info!("engine killed, cleaning up");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let reese_car_engine = Engine::new();
 }
